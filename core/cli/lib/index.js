18cec1badfc69936d72d7ad2c33ba72b3a08924f
phm-front <506129591@qq.com>
2023-05-09 22:42:17 +0800

    重构项目结构

---
+++ core/cli/lib/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = main;
+
+function main() {
+  console.log('core exec')
+  return 'Hello from core';
+}
--- packages/core/bin/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const utils = require('@peui-cli/utils')
-console.log(utils())
-console.log('peui-cli')
-console.log('pnpm publish')
